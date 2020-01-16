897ea76333c53de07e4edc0b77b84661ef8b7a2b
Matt Gardner <wilburnforce@gmail.com>
2020-01-16 10:08:56 -0500

    Convert proposed class property arrow funcs;
    I was having trouble getting this to build because babel did not like the syntax of the class property _arrow functions_. Other ES features work.
    
    Instead I just moved them to the constructor until we can understand why this babel feature isn’t working.

--- index.js
+++ index.js
@@ -20,70 +20,70 @@ export default class MapboxAccessibility {
     }
 
     this.options = xtend(defaultOptions, options);
-  }
 
-  clearMarkers = () => {
-    if (this.features) {
-      this.features.forEach(feature => {
-        if (feature.marker) {
-          this.map.getCanvasContainer().removeChild(feature.marker);
-          delete feature.marker;
-        }
-      });
-    }
-  };
-
-  queryFeatures = () => {
-    this._debouncedQueryFeatures.cancel();
-    this.clearMarkers();
-
-    this.features = this.map.queryRenderedFeatures({ layers: this.options.layers });
-    this.features.map((feature) => {
-      let { width, height } = this.options;
-      const label = feature.properties[this.options.accessibleLabelProperty];
-
-      feature.marker = document.createElement('button');
-      feature.marker.setAttribute('aria-label', label);
-      feature.marker.setAttribute('title', label);
-      feature.marker.setAttribute('tabindex', 0);
-      feature.marker.style.display = 'block';
-
-      let position;
-      if (feature.geometry.type === 'Point') {
-        position = this.map.project(feature.geometry.coordinates);
-      } else {
-        const featureBbox = bbox(feature);
-        const bl = this.map.project([featureBbox[0], featureBbox[1]]);
-        const tr = this.map.project([featureBbox[2], featureBbox[3]]);
-
-        width = Math.abs(tr.x - bl.x);
-        height = Math.abs(tr.y - bl.y);
-
-        position = {
-          x: ((tr.x + bl.x) / 2),
-          y: ((tr.y + bl.y) / 2),
-        };
+    this.clearMarkers = () => {
+      if (this.features) {
+        this.features.forEach(feature => {
+          if (feature.marker) {
+            this.map.getCanvasContainer().removeChild(feature.marker);
+            delete feature.marker;
+          }
+        });
       }
-      feature.marker.style.width = `${width}px`;
-      feature.marker.style.height = `${height}px`;
-      feature.marker.style.transform = `translate(-50%, -50%) translate(${position.x}px, ${position.y}px)`;
-      feature.marker.className = 'mapboxgl-accessibility-marker';
+    };
 
-      this.map.getCanvasContainer().appendChild(feature.marker);
-      return feature;
-    });
-  };
+    this.queryFeatures = () => {
+      this._debouncedQueryFeatures.cancel();
+      this.clearMarkers();
 
-  _movestart = () => {
-    this._debouncedQueryFeatures.cancel();
-    this.clearMarkers();
-  };
+      this.features = this.map.queryRenderedFeatures({ layers: this.options.layers });
+      this.features.map((feature) => {
+        let { width, height } = this.options;
+        const label = feature.properties[this.options.accessibleLabelProperty];
 
-  _render = () => {
-    if (!this.map.isMoving()) {
-      this._debouncedQueryFeatures();
-    }
-  };
+        feature.marker = document.createElement('button');
+        feature.marker.setAttribute('aria-label', label);
+        feature.marker.setAttribute('title', label);
+        feature.marker.setAttribute('tabindex', 0);
+        feature.marker.style.display = 'block';
+
+        let position;
+        if (feature.geometry.type === 'Point') {
+          position = this.map.project(feature.geometry.coordinates);
+        } else {
+          const featureBbox = bbox(feature);
+          const bl = this.map.project([featureBbox[0], featureBbox[1]]);
+          const tr = this.map.project([featureBbox[2], featureBbox[3]]);
+
+          width = Math.abs(tr.x - bl.x);
+          height = Math.abs(tr.y - bl.y);
+
+          position = {
+            x: ((tr.x + bl.x) / 2),
+            y: ((tr.y + bl.y) / 2),
+          };
+        }
+        feature.marker.style.width = `${width}px`;
+        feature.marker.style.height = `${height}px`;
+        feature.marker.style.transform = `translate(-50%, -50%) translate(${position.x}px, ${position.y}px)`;
+        feature.marker.className = 'mapboxgl-accessibility-marker';
+
+        this.map.getCanvasContainer().appendChild(feature.marker);
+        return feature;
+      });
+    };
+
+    this._movestart = () => {
+      this._debouncedQueryFeatures.cancel();
+      this.clearMarkers();
+    };
+
+    this._render = () => {
+      if (!this.map.isMoving()) {
+        this._debouncedQueryFeatures();
+      }
+    };
+  }
 
   onAdd(map) {
     this.map = map;
